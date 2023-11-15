56717f72195bf4d8329386e772ed7f136d3d7515
Chumki khatun <chumkikhatun370@gamil.com>
2023-11-15 17:13:15 +0600

    added delete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -51,6 +51,13 @@ async function run() {
       const email = req.query.email;
       const query = {email :email}
       const result  = await cartsCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/carts/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
       res.send(result)
     })
 
